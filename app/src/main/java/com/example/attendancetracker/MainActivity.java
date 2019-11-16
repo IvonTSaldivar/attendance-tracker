84c89b47a7b16a3f85a95505e8ca7b3844445129
Ivon Saldivar <saldivar.ivon@ichsstudent.org>
2019-11-15 23:08:09 -0700

    Second push

--- app/src/main/java/com/example/attendancetracker/MainActivity.java
+++ app/src/main/java/com/example/attendancetracker/MainActivity.java
@@ -1,12 +1,17 @@
 package com.example.attendancetracker;
 
 import android.os.Bundle;
+import android.provider.Settings;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.support.annotation.NonNull;
 import android.view.MenuItem;
 import android.widget.TextView;
 
+/**
+ * Todo
+ *
+ */
 public class MainActivity extends AppCompatActivity {
     private TextView mTextMessage;
 
@@ -26,6 +31,7 @@ public class MainActivity extends AppCompatActivity {
                     mTextMessage.setText(R.string.title_notifications);
                     return true;
             }
+
             return false;
         }
     };
